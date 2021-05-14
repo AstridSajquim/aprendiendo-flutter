b44c7417e9f489d41e33fb8d0e7c09ba19cdf9f1
Astrid <asajquim@deltatracking.com>
2021-05-13 19:20:59 -0600

    widget ordenados

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -24,29 +24,71 @@ class _InicioState extends State<Inicio> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("MiApp"),
-        ),
-        body: Center(
-          child: RaisedButton(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Icon(Icons.access_time),
-                SizedBox(
-                  height: 5.0,
-                ),
-                Text(
-                  "hola",
-                  textAlign: TextAlign.center,
-                )
-              ],
-            ),
-            onPressed: () {
-              var t = DateTime.now();
-              print(t);
-            },
-          ),
-        ));
+      body: cuerpo(),
+    );
   }
+}
+
+Widget cuerpo() {
+  return Container(
+    decoration: BoxDecoration(
+        image: DecorationImage(
+            image: NetworkImage(
+                "https://www.grupoarco.com.mx/wp-content/uploads/fondo-amarillo-300x118.jpg"),
+            fit: BoxFit.cover)),
+    child: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          nombre(),
+          usuario(),
+          password(),
+          SizedBox(
+            height: 10.0,
+          ),
+          boton()
+        ],
+      ),
+    ),
+  );
+}
+
+Widget nombre() {
+  return Text(
+    "Ingrese sus datos",
+    style: TextStyle(
+        color: Colors.brown, fontSize: 35.0, fontWeight: FontWeight.bold),
+  );
+}
+
+Widget usuario() {
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 5.0),
+    child: TextField(
+      decoration: InputDecoration(
+          hintText: "User", fillColor: Colors.white, filled: true),
+    ),
+  );
+}
+
+Widget password() {
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 5.0),
+    child: TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+          hintText: "Password", fillColor: Colors.white, filled: true),
+    ),
+  );
+}
+
+Widget boton() {
+  return FlatButton(
+      color: Colors.white,
+      padding: EdgeInsets.symmetric(horizontal: 150.0, vertical: 10.0),
+      onPressed: () {},
+      child: Text(
+        "Ok",
+        style: TextStyle(fontSize: 25.0, color: Colors.brown),
+      ));
 }
